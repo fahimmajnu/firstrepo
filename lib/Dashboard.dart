17f8eac87d0923145f7b4a6990f7e1d7426ddeaa
fahimmajnu <majnufahim@gmail.com>
2022-01-25 14:17:34 +0500

    first commit

---
+++ lib/Dashboard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Dashboard extends StatefulWidget {
+  
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children:<Widget> [
+        
+
+      ],
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:new_app/Dashboard.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+ 
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dashboard(),
+      
+    );
+  }
+}
